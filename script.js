e65cf2e5e92ff9c6a8897607adc9b984d55097bd
Gamuchirai15 <gamiechif@gmail.com>
2024-10-08 16:23:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-document.addEventListener('DOMContentLoaded'.function())
-document.getElementById(registration-form)
-document.getElementById(form-feedback)
-document.getElementById(username)
-document.getElementById(email)
-document.getElementById(password)
-document.getElementById(trim)
+document.addEventListener('DOMContentLoaded'.function());
+document.getElementById(registration-form);
+document.getElementById(form-feedback);
 
+document.addEventListener('submit');
+function register(submit) {
+    event.preventDefault()
+};
+
+document.getElementById(username);
+document.getElementById(email);
+document.getElementById(password);
+
+const username = "username";
+const email = "email";
+const password = "password";
+
+const value = username.trim();
+const string = email.trim();
+const str = password.trim();
