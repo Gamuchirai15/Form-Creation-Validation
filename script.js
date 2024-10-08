358e23214746fa15e8e50be3dc8ebe328b344726
Gamuchirai15 <gamiechif@gmail.com>
2024-10-08 17:11:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -27,10 +27,18 @@ if (username.length<3) {
     return !isValid
 }else {
     messages = isValid
-}
+};
 
 if (password.length>8) {
     return isValid
 }else {
     messages = !isValid
-}
+};
+
+const btn = document.querySelector(".button");
+const feedback = document.querySelector(".feedback-container");
+btn.addEventListener("click", () => {
+    feedback.innerHTML = '<h1>Thank you for your feedback</h1>';
+});
+
+feedbackDiv.setAttribute("style", "style.display=block");
